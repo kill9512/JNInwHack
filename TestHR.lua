c52758cc692e66f2672055942b4f0bf9473eec82
kill9512 <88079302+kill9512@users.noreply.github.com>
2024-01-15 00:09:20 +0700

    Revert "Update TestHR.lua"
    This reverts commit 358d84a3eea6b2e776c47f9113730e4df03c9445.

--- TestHR.lua
+++ TestHR.lua
@@ -66,14 +66,3 @@ Section:NewButton("Copy Positions", "Copy positions to clipboard", function()
         setclipboard(positionString)
     end
 end)
-
-local positionsTextBox = Section:NewTextBox("Paste Positions", "Paste positions here", function(text)
-    local newPositions = {}
-    for line in text:gmatch("[^\r\n]+") do
-        local x, y, z = line:match("Vector3.new%((.-), (.-), (.-)%)")
-        if x and y and z then
-            table.insert(newPositions, Vector3.new(tonumber(x), tonumber(y), tonumber(z)))
-        end
-    end
-    targetPositions = newPositions
-end)
